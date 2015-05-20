c7d74c34900ec8e4c43c89e8843308b680576dde
David Brelloch <brelloch@gmail.com>
2015-05-20 19:25:41 -0400

    Added some stuff for resumator

--- JobBoardWorker/JobBoardConsole/Program.cs
+++ JobBoardWorker/JobBoardConsole/Program.cs
@@ -22,63 +22,70 @@ namespace JobBoardConsole
                 List<Job> alljobs = new List<Job>();
 
                 ///////////// Resumator
-                var query = from b in db.Companies
+                var companies = from b in db.Companies
                             orderby b.Name
                             select b;
-                
-                foreach (var item in query)
+
+                Parallel.ForEach(companies, item =>
                 {
                     if (item.Type == JobBoardType.TheResumator)
                     {
-                        var task = TheResumatorBoardParser.Parse(item.Name, item.Url);
-                        task.Wait();
-                        var jobs = task.Result;
-                        alljobs.AddRange(jobs);
+                        try
+                        {
+                            var task = TheResumatorBoardParser.Parse(item.Name, item.Url);
+                            task.Wait();
+                            var jobs = task.Result;
+                            alljobs.AddRange(jobs);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
                     }
-                    Console.WriteLine(item.Name);
-                }
+                    Console.WriteLine(item.CompanyId);
+                });
 
                 foreach (var job in alljobs) {
                     db.Jobs.Add(job);
                 }
                 db.SaveChanges();
 
-                /////////////// Jobvite
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri("https://search.jobvite.com/api/jobsearch?limit=" + 1 + "&start=" + 0);
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                    var responseTask = client.GetAsync("");
-                    responseTask.Wait();
-                    HttpResponseMessage response = responseTask.Result;
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var contentTask = response.Content.ReadAsStringAsync();
-                        contentTask.Wait();
-                        var content = contentTask.Result.Replace("\r\n", "");
-                        var totalJobs = int.Parse(Newtonsoft.Json.JsonConvert.DeserializeObject<JobviteBase>(content).totalResults);
-                        totalJobs = 10;
-                        //int pageSize = 100;
-                        int pageSize = 10;
-
-                        for (int i = 0; (i-1 * pageSize) < totalJobs; i++)
-                        {
-                            var task = JobviteParser.Parse(i*pageSize, pageSize);
-                            task.Wait();
-                            var jobs = task.Result;
-
-                            foreach (var job in jobs)
-                            {
-                                db.Jobs.Add(job);
-                            }
-                            db.SaveChanges();
-
-                        }
-                    }
-                }
+                ///////////////// Jobvite
+                //using (var client = new HttpClient())
+                //{
+                //    client.BaseAddress = new Uri("https://search.jobvite.com/api/jobsearch?limit=" + 1 + "&start=" + 0);
+                //    client.DefaultRequestHeaders.Accept.Clear();
+                //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //
+                //    var responseTask = client.GetAsync("");
+                //    responseTask.Wait();
+                //    HttpResponseMessage response = responseTask.Result;
+                //
+                //    if (response.IsSuccessStatusCode)
+                //    {
+                //        var contentTask = response.Content.ReadAsStringAsync();
+                //        contentTask.Wait();
+                //        var content = contentTask.Result.Replace("\r\n", "");
+                //        var totalJobs = int.Parse(Newtonsoft.Json.JsonConvert.DeserializeObject<JobviteBase>(content).totalResults);
+                //        totalJobs = 10;
+                //        //int pageSize = 100;
+                //        int pageSize = 10;
+                //
+                //        for (int i = 0; (i-1 * pageSize) < totalJobs; i++)
+                //        {
+                //            var task = JobviteParser.Parse(i*pageSize, pageSize);
+                //            task.Wait();
+                //            var jobs = task.Result;
+                //
+                //            foreach (var job in jobs)
+                //            {
+                //                db.Jobs.Add(job);
+                //            }
+                //            db.SaveChanges();
+                //
+                //        }
+                //    }
+                //}
 
                 Console.WriteLine("Press any key to exit...");
                 Console.ReadKey();
---
+++ JobBoardWorker/SearchTool/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Customsearch;
+using Google.Apis.Services;
+
+namespace SearchTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiKey = "AIzaSyBYCdftWAN-sk-vkc6hhmrHcTl4yaHRhPQ";
+            string cx = "011210479108868451169:4uy7jqr3igk";
+            string query = "site:*.theresumator.com";
+
+
+            var svc = new Google.Apis.Customsearch.v1.CustomsearchService(new BaseClientService.Initializer { ApiKey = apiKey });
+            var listRequest = svc.Cse.List(query);
+            int page = 0;
+            List<string> urls = new List<string>();
+
+            string startDate = DateTime.UtcNow.AddDays(-90).ToString("yyyyMMdd");
+            //string startDate = (DateTime.UtcNow.Year.ToString() + sMonth + sday);
+            string endDate = DateTime.UtcNow.AddDays(-60).ToString("yyyyMMdd");
+            listRequest.DateRestrict = "w10";
+
+            try
+            {
+                while (page < 10)
+                {
+                    listRequest.Num = 10;
+                    listRequest.Start = page * listRequest.Num;
+
+                    if (listRequest.Start == 0)
+                        listRequest.Start = 1;
+
+                    listRequest.Cx = cx;
+                    var search = listRequest.Execute();
+
+                    foreach (var result in search.Items)
+                    {
+                        urls.Add(result.Link);
+                        Console.WriteLine("Title: {0}", result.Title);
+                        Console.WriteLine("Link: {0}", result.Link);
+                    }
+                    page++;
+                }
+            }
+            catch (Exception ex)
+            {
+                var test = ex.Message;
+            }
+
+            System.IO.File.WriteAllLines(@"C:\urlsa.txt", urls.Distinct().ToArray());
+
+            Console.WriteLine("done");
+        }
+    }
+}
